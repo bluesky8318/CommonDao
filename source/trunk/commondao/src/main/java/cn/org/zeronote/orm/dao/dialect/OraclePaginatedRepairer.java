f3b2caa85aafb7c951eec49c50ed6547879fb711
leon <lizheng8318@gmail.com>
2014-03-31 16:53:06 +0800

    完成oracle分页查询

--- source/trunk/commondao/src/main/java/cn/org/zeronote/orm/dao/dialect/OraclePaginatedRepairer.java
+++ source/trunk/commondao/src/main/java/cn/org/zeronote/orm/dao/dialect/OraclePaginatedRepairer.java
@@ -3,11 +3,17 @@
  */
 package cn.org.zeronote.orm.dao.dialect;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.sql.DataSource;
+
+import org.apache.commons.dbutils.handlers.ScalarHandler;
 
 import cn.org.zeronote.orm.DataAccessException;
 import cn.org.zeronote.orm.PaginationSupport;
 import cn.org.zeronote.orm.RowSelection;
+import cn.org.zeronote.orm.extractor.PojoListResultSetExtractor;
 
 /**
  * oracle 分页查询
@@ -31,9 +37,49 @@ public class OraclePaginatedRepairer extends AbstractPaginatedRepairer {
 			DataSource dataSource, String sql, Object[] args,
 			Class<T> pojoType, RowSelection rowSelection)
 			throws DataAccessException {
-		// 调整sql语句，添加rownum进行分页
+		// 将sql语句转换成对应的 查询总数/查询当前页 两个sql
+		// 总数sql
+		StringBuilder cSql = new StringBuilder("select count(1) from (");
+		cSql.append(sql)
+			.append(") mt");
 		
-		return null;
+		// 分页sql
+		StringBuilder nSql = new StringBuilder("select * from (");
+		nSql.append("select ROWNUM rw, mt.* from (")
+			.append(sql)
+			.append("ORDER BY ")
+			.append(rowSelection.getOrder())
+			.append(") mt ");
+		nSql.append(") mst where mst.rw between ? and ? ");
+		
+		PaginationSupport<T> ps = new PaginationSupport<T>();
+		// 计算count
+		Integer count = query(dataSource, cSql.toString(), args, new ScalarHandler<Integer>());
+		ps.setTotalCount(count);
+		ps.setPageSize(rowSelection.getPageSize());
+		ps.setPageCount((int) (ps.getTotalCount() / ps.getPageSize() + (ps.getTotalCount() % ps.getPageSize() == 0 ? 0 : 1 )));
+		
+		int ccr = rowSelection.getStartPage() * rowSelection.getPageSize();
+		if (ps.getTotalCount() <= ccr) {
+			// 请求的当前页，没有数据
+			ps.setCurrentPage(rowSelection.getStartPage());
+			ps.setObject(new ArrayList<T>());
+		} else {
+			// 读取当前页数据
+			ps.setCurrentPage(rowSelection.getStartPage());
+			// 拼接参数，between的两端
+			Object[] nArgs = new Object[args.length + 2];
+			for (int i = 0; i < args.length; i++) {
+				nArgs[i] = args[i];
+			}
+			nArgs[args.length] = ccr + 1;
+			nArgs[args.length + 1] = ccr + rowSelection.getPageSize();
+			
+			List<T> ls = query(dataSource, nSql.toString(), nArgs, new PojoListResultSetExtractor<T>(pojoType));
+			ps.setObject(ls);
+		}
+		
+		return ps;
 	}
 
 }
